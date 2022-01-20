3a2a1287df06777f3236f2b6deac664bfb8cd28a
HofitVardi <98025975+HofitVardi@users.noreply.github.com>
2022-01-20 18:02:15 +0200

    Add files via upload

---
+++ LinearLR.py
@@ -0,0 +1,41 @@
+from os import error
+import numpy as np
+
+
+def AvScheme(f,num_iterations=1,periodic=True):
+    if (num_iterations==0):
+        return f
+    else:
+        new = []
+        for i in range(len(f)-1):
+            new.append(f[i])
+            new.append((f[i]+f[i+1])/2)
+        new.append(f[len(f)-1])
+        if (periodic):
+            new.append((f[len(f)-1]+f[0])/2)
+        return AvScheme(np.array(new),num_iterations-1,periodic)
+
+
+def smoothing(f,num_iterations,periodic=True):
+    if (num_iterations<1):
+        return f
+    else:
+        if (len(f)<2):
+            error("not enough data")
+        new = []
+        for i in range(len(f)-1):
+            new.append((f[i]+f[i+1])/2)
+        if (periodic):
+            new.append((f[len(f)-1]+f[0])/2)
+        return smoothing(np.array(new),num_iterations-1,periodic)
+
+
+
+# Lane-Risenfeld with m-1 smoothing steps
+def LR(m,f,iterations=8,periodic=True):
+    if (iterations==0):
+        return f
+    else:
+        f = AvScheme(f,1,periodic) #refinement step
+        f = smoothing(f,m-1,periodic)
+        return LR(m,f,iterations-1,periodic)
